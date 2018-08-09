1bd0944945f542bfd97c15c880d5e20909853d3f
Artem Laptev <artemlaptev63@MacBook-Air-Artem.local>
2018-08-09 15:15:13 +0400

    Added billing and client side app!

---
+++ client/src/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+export default function configureStore() {
+  let store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+  return store;
+}
---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import configureStore from './configureStore';
+import App from './components/App';
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
